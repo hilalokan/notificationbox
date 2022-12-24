04a30906c1659efcfda1c59b17dbb8112c3c1110
hilalokan <91684373+hilalokan@users.noreply.github.com>
2022-12-24 17:35:17 +0300

    notification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react";
+import Table from "react-bootstrap/Table";
 
-function App() {
+const App = () => {
+  const reports = [
+    {name: "Hilal Okan", number: "18290119", text:"Objected to the score."},
+    {name: "Bertuğ Kara", number: "18290104", text:"Objected to the score."},
+    {name: "Osman Buğra Dağlı", number:"18290092", text:"Objected to the score."},
+    {name: "Kerem Ali Öztürk", number: "19290792", text:"Objected to the score."}
+  ]
+
+  const renderReports = (reports,index) => {
+    return (
+      <tr key ={index}>
+        <td>{reports.name}</td>
+        <td>{reports.number}</td>
+        <td>{reports.text}</td>
+      </tr>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h5>NOTIFICATIONS</h5>
+      <hr></hr>
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Student ID</th>
+          <th>Message</th>
+        </tr>
+      </thead>
+   <tbody>
+      {reports.map(renderReports)}
+   </tbody>
+    </Table>
     </div>
   );
+
 }
+  
 
 export default App;
